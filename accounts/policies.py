eb38544a97885dd2b1de1644aa3805d465c5e77f
Adam Terrey <arterrey@gmail.com>
2015-07-23 16:01:21 +0700

    Pylinted stuff

--- accounts/policies.py
+++ accounts/policies.py
@@ -1,20 +1,22 @@
-
+# pylint: disable=import-error,missing-docstring,unused-argument
 
 from decimal import Decimal
 from ap.accounts.policy import policy
+from ap.accounts.Entry import Entry
+from ap.accounts import DEBIT, CREDIT
 
 
 
 
-@policy(is_has=('inc-gst','purchase'))
+@policy(is_has=('inc-gst', 'purchase'))
 def payed_gst_on_purchase(context, data):
     gst_payed = data["amount"]
-    transaction.add_entry("liabilities.gst", Entry(DEBIT, gst_payed))
-    transaction.add_entry("equity.retained_profets", Entry(CREDIT, gst_payed))
+    context.transaction.add_entry("liabilities.gst", Entry(DEBIT, gst_payed))
+    context.transaction.add_entry("equity.retained_profets", Entry(CREDIT, gst_payed))
 
-@test(payed_gst_on_purchase, {
-        'amount': Decimal("11.00")  
-    })
+@payed_gst_on_purchase.test(payed_gst_on_purchase, {
+    'amount': Decimal("11.00")
+})
 def test_payed_gst_on_purchase(context, data):
     assert context.transaction.entries["liabilities.gst"] == Entry(CREDIT, Decimal("1"))
     assert context.transaction.entries["equity.retained_profets"] == Entry(DEBIT, Decimal("1"))
@@ -24,7 +26,7 @@ def test_payed_gst_on_purchase(context, data):
 
 
 
-@policy(is_has=('cost-of-sales','purchase'))
+@policy(is_has=('cost-of-sales', 'purchase'))
 def tax_cost_of_sales(context, data):
     context.transaction["tax.expense.cost-of-sales"] = data["amount"]
 
@@ -32,5 +34,5 @@ def tax_cost_of_sales(context, data):
 @policy(is_has=('purchase',))
 def purchase(context, data):
     amount = data["amount"]
-    transaction.add_entry("assets.cash", Entry(CREDIT, amount))
-    transaction.add_entry("equity.retained_profets", Entry(DEBIT, amount))
+    context.transaction.add_entry("assets.cash", Entry(CREDIT, amount))
+    context.transaction.add_entry("equity.retained_profets", Entry(DEBIT, amount))
